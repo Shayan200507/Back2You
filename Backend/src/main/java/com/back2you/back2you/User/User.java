625a4cea8f71222d482eae22d0cdaeea017846ba
Shayan Sheikh <shayan.faisal1095@gmail.com>
2026-09-05 23:49:10 -0400

    backend

--- Backend/src/main/java/com/back2you/back2you/User/User.java
+++ Backend/src/main/java/com/back2you/back2you/User/User.java
@@ -21,7 +21,7 @@ public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private  Integer ID;
+    private  Integer /status;
 
     private LocalDate birthDate;
 
